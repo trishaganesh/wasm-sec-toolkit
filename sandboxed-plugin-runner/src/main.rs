c28f2f859e041c2c9004e8ffe7b3eea9178cd8ed
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-05 21:33:36 -0800

    Created a linker and defined a host function so the Wasm module can send strings to the Rust console

--- sandboxed-plugin-runner/src/main.rs
+++ sandboxed-plugin-runner/src/main.rs
@@ -15,6 +15,22 @@ fn main() -> anyhow::Result<()> {
     let engine = Engine::default();
     let mut store = Store::new(&engine, ());
 
+    //creating a new linker associated with the Wasmtime engine
+    let mut linker = Linker::new(&engine);
+
+    /*defining a host function "host_print" in the "env" module allowing the
+     Wasm module to call this function */
+    linker.func_wrap("env", "host_print", |msg_ptr: i32, msg_len: i32, memory: Memory| {
+        //getting the WebAssembly module's linear memory
+        let data = memory.data(&store);
+        //extracting the bytes corresponding to the message from memory
+        let bytes = &data[msg_ptr as usize..(msg_ptr + msg_len) as usize];
+        //converting the bytes to a UTF-8 string, if possible
+        if let Ok(msg) = std::str::from_utf8(bytes) {
+            println!("[PLUGIN]: {}", msg);
+        }
+    })?;
+    
     //loading the WASM plugin
     let module = Module::from_file(&engine, "plugins/example.wasm")?;
 
