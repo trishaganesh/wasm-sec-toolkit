92241bd6437fb9069d5423ad977b7e6946ac921d
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-22 14:40:28 -0800

    Update index.js

--- wasm-waf/node/index.js
+++ wasm-waf/node/index.js
@@ -1,4 +1,7 @@
 //the node.js file system module to read files
 import fs from "fs";
 
+//then read the compiled WebAssembly firewall module (.wasm) into a buffer
+const wasmBuffer = fs.readFileSync("../dist/wasm_waf.wasm");
+
 
