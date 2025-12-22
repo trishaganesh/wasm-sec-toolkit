e701f49cfa9d9fb7852a7b2f21b77d04cf26c5b3
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-22 13:51:25 -0800

    Add validate email, reg, and unwrap

--- wasm-input-firewall/wasm-firewall/src/lib.rs
+++ wasm-input-firewall/wasm-firewall/src/lib.rs
@@ -8,3 +8,15 @@ use regex::Regex;
 exposed to JavaScript via wasm-bindgen
 this returns true if the email matches the regex pattern */
 #[wasm_bindgen]
+pub fn validate_email(email: &str) -> bool {
+    /*
+ - Allows alphanumeric characters and common symbols
+- Requires an '@' symbol
+- Requires a valid domain and TLD (2+ characters) */
+    let reg = Regex::new(
+        r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+    ).unwrap(); //this is a safe unwrap: regex is static and known-valid
+
+    //need to check if the input email matches the pattern
+    reg.is_match(email)
+}
