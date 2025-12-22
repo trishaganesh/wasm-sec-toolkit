01e776ad1af63a9bd20a59525ff9b275028e5ed7
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-22 14:44:44 -0800

    Update main.ts

--- wasm-waf/deno/main.ts
+++ wasm-waf/deno/main.ts
@@ -3,3 +3,6 @@ const wasm = await WebAssembly.instantiate(
   await Deno.readFile("../dist/wasm_waf.wasm"),
   {} //there are no imports required and WASM runs fully sandboxed
 );
+
+//we need to also xtract the payload inspection function from WASM exports
+const { inspect_payload } = wasm.instance.exports as any;
