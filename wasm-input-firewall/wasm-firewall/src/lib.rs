810504b5b3f8952f457cd138df4dbe1b58670e67
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-22 13:49:38 -0800

    Update lib.rs

--- wasm-input-firewall/wasm-firewall/src/lib.rs
+++ wasm-input-firewall/wasm-firewall/src/lib.rs
@@ -4,3 +4,7 @@ use wasm_bindgen::prelude::*;
 //regex is used for pattern matching (email + SQL injection detection)
 use regex::Regex;
 
+/* need to validate whether a string is a properly formatted email address
+exposed to JavaScript via wasm-bindgen
+this returns true if the email matches the regex pattern */
+#[wasm_bindgen]
