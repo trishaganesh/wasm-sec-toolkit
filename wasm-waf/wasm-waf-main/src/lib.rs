b3f011f1549288227a1ed36dc083685bdee0c75d
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-22 14:27:50 -0800

    Update lib.rs

--- wasm-waf/wasm-waf-main/src/lib.rs
+++ wasm-waf/wasm-waf-main/src/lib.rs
@@ -2,6 +2,7 @@
 use wasm_bindgen::prelude::*;
 //the regex library for pattern matching (used to match rules against payloads)
 use regex::Regex;
+//serde for JSON serialization/deserialization
 use serde::Deserialize;
 
 #[derive(Deserialize)]
