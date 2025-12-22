fc2523fc0e85e732dd393658cfc67272d9fe6ad7
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-12-22 13:48:56 -0800

    Create wasm_bindgen and regex in lib.rs

---
+++ wasm-input-firewall/wasm-firewall/src/lib.rs
@@ -0,0 +1,6 @@
+//import wasm-bindgen so these functions can be called from JavaScript
+use wasm_bindgen::prelude::*;
+
+//regex is used for pattern matching (email + SQL injection detection)
+use regex::Regex;
+
