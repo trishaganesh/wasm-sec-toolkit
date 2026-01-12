eb01972adc9bfee43650bea002fae3df71d4cd32
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2026-01-11 20:49:56 -0800

    Add argon2

--- wasm-password-hashing/src/lib.rs
+++ wasm-password-hashing/src/lib.rs
@@ -1,2 +1,5 @@
 //need to import the wasm-bindgen prelude so we can expose Rust functions to JavaScript
 use wasm_bindgen::prelude::*;
+
+//need to import Argon2 password hashing library and its configuration struct
+use argon2::{self, Config};
