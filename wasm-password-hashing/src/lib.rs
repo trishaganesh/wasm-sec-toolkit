6100ed392060e953df8e62be7805c07c5de9a3e0
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2026-01-11 20:52:47 -0800

    Add comment for following code

--- wasm-password-hashing/src/lib.rs
+++ wasm-password-hashing/src/lib.rs
@@ -3,3 +3,18 @@ use wasm_bindgen::prelude::*;
 
 //need to import Argon2 password hashing library and its configuration struct
 use argon2::{self, Config};
+
+/*  
+Do the following:
+Hash a password using Argon2id and return an encoded hash string.
+
+This function is exposed to JavaScript via wasm-bindgen.
+
+Parameters:
+- password: The plaintext password provided by the user
+- salt: A cryptographically random salt (should be unique per user)
+
+ Returns:
+- A PHC-formatted Argon2 hash string containing:
+  algorithm, version, parameters, salt, and hash
+*/
